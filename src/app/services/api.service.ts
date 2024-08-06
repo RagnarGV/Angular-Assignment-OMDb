db2d1aa5c03f992b47d87420a2bea5d3dcfd16cf
Gaurav Hariyani <gauravhariyani12.gh@gmail.com>
2024-08-05 21:48:35 -0400

    [Second Build/HTTPS fix]

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
 })
 export class ApiService {
   // private apiUrl = ' http://www.omdbapi.com/?i=tt3896198&apikey=37444360'; // Example public API
-  private apiUrl = ' http://www.omdbapi.com/?apikey=37444360&'; // Example public API
+  private apiUrl = ' https://www.omdbapi.com/?apikey=37444360&'; // Example public API
   constructor(private http: HttpClient) {}
 
   getData(title: any, year: any): Observable<any> {
